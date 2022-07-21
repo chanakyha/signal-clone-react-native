94612c46646a66fb5d354783da22877ce560be3e
Chanakyha <vetrichanakyha2003@gmail.com>
2022-07-22 00:55:27 +0530

    Added React Native Navigation

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBpnhizqPPEdBhIrcBmMjIls2PE-gs4Wws",
+  authDomain: "signal-clone-ffed4.firebaseapp.com",
+  projectId: "signal-clone-ffed4",
+  storageBucket: "signal-clone-ffed4.appspot.com",
+  messagingSenderId: "91199030810",
+  appId: "1:91199030810:web:6766695b3d984b71a133a3",
+};
