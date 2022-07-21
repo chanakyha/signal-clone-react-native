aff84bb04fb67ca793c64d6222c184513b19a546
Chanakyha <vetrichanakyha2003@gmail.com>
2022-07-22 01:38:40 +0530

    Imported Required Firebase files

--- firebase.js
+++ firebase.js
@@ -1,3 +1,7 @@
+import * as firebase from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBpnhizqPPEdBhIrcBmMjIls2PE-gs4Wws",
   authDomain: "signal-clone-ffed4.firebaseapp.com",
@@ -6,3 +10,15 @@ const firebaseConfig = {
   messagingSenderId: "91199030810",
   appId: "1:91199030810:web:6766695b3d984b71a133a3",
 };
+
+let app;
+if (firebaseConfig.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app();
+}
+
+const db = app.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
