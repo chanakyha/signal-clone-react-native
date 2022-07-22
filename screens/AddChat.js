38eb0e6c696f3126e5685bdf5b9dfc50a723aec4
Chanakyha <vetrichanakyha2003@gmail.com>
2022-07-22 16:29:10 +0530

    Add New Chats Page is Done

--- screens/AddChat.js
+++ screens/AddChat.js
@@ -1,14 +1,52 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useLayoutEffect, useState } from "react";
+import { Button, Input } from "@rneui/base";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { db } from "../firebase";
 
-const AddChat = () => {
+const AddChat = ({ navigation }) => {
+  const [input, setInput] = useState("");
+
+  const createChat = async () => {
+    await db
+      .collection("chats")
+      .add({
+        chatName: input,
+      })
+      .then(() => {
+        navigation.goBack();
+      })
+      .error(alert);
+  };
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "Add a New Chat",
+      headerBackTitle: "Chats",
+    });
+  }, [navigation]);
+
   return (
-    <View>
-      <Text>AddChat</Text>
+    <View style={styles.container}>
+      <Input
+        placeholder="Enter a Chat Name"
+        value={input}
+        onChangeText={(text) => setInput(text)}
+        leftIcon={
+          <Icon name="wechat" type="antdesign" size={24} color="black" />
+        }
+      />
+      <Button title="Create New Chat" onPress={createChat} />
     </View>
   );
 };
 
 export default AddChat;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 30,
+    height: "100%",
+  },
+});
